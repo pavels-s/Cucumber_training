a8076b7a5d46103fd7f8011b302008a418a28534
pavels-s <pavels.sarkels@gmail.com>
2022-10-05 12:03:03 +0300

    First commit with project template and dependencies

---
+++ Cucumber_training/src/main/java/Automation/Cucumber_training/App.java
@@ -0,0 +1,13 @@
+package Automation.Cucumber_training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
