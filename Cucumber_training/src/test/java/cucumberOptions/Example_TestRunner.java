05dd69c860b87bf837a7ea7924b0ebd26f4683e1
pavels-s <pavels.sarkels@gmail.com>
2022-10-13 13:47:24 +0300

    Comments at Test Runner

--- Cucumber_training/src/test/java/cucumberOptions/Example_TestRunner.java
+++ Cucumber_training/src/test/java/cucumberOptions/Example_TestRunner.java
@@ -11,8 +11,14 @@ import io.cucumber.junit.CucumberOptions;
 		//glue = parameter of step definition file
 		glue = "stepDefinitions", 
 		stepNotifications = true, 
+		//example: "not @RegTest" "@SmokeTest and(or) @RegTest"
 		tags = "@PortalTest",
-		plugin = {"pretty", "html:target/cucumber.html"}) /* example: "not @RegTest" "@SmokeTest and(or) @RegTest */
+		//Way to validate if all steps are properly mapped in step definition
+		dryRun = true,
+		//Way to make console results looks more attractive
+		monochrome = true,
+		//things to generate a specific reports in given directory
+		plugin = {"pretty", "html:target/cucumber_html_report.html", "json:target/cucumber_json_report.json", "junit:target/cucumber_xml_report.xml"}) 
 
 public class Example_TestRunner {
 
