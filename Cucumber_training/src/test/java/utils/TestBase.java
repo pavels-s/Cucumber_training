b080b07805cb68b0d5bb5932de39e9d156170f11
pavels-s <pavels.sarkels@gmail.com>
2022-10-18 13:41:10 +0300

    Global properties implementation

--- Cucumber_training/src/test/java/utils/TestBase.java
+++ Cucumber_training/src/test/java/utils/TestBase.java
@@ -1,5 +1,10 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,12 +12,20 @@ public class TestBase {
 
 	public WebDriver driver;
 	
-	public WebDriver WebDriverManager() {
+	public WebDriver WebDriverManager() throws IOException {
+		
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//global.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		String url = prop.getProperty("QAUrl");
 		
 		if(driver == null) {
-		System.setProperty("webdriver.chrome.driver","/JavaGuru/Soft/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+			if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver","/JavaGuru/Soft/chromedriver.exe");
+				driver = new ChromeDriver();
+			}
+			
+		driver.get(url);
 		}
 		return driver;
 	}
