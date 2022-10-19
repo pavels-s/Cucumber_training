cbc89401e818177336047c7bea58dd9b206535bc
pavels-s <pavels.sarkels@gmail.com>
2022-10-19 16:16:22 +0300

    Failure screenshot implementation

--- Cucumber_training/src/test/java/stepDefinitions/Hooks.java
+++ Cucumber_training/src/test/java/stepDefinitions/Hooks.java
@@ -1,8 +1,18 @@
 package stepDefinitions;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+//import com.aventstack.extentreports.gherkin.model.Scenario;
+
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.AfterStep;
 import utils.TestContextSetup;
 
 public class Hooks {
@@ -17,7 +27,15 @@ public class Hooks {
 		testContextSetup.testBase.WebDriverManager().quit();
 	}
 	
-
+	@AfterStep
+	public void addScreenshot(Scenario scenario) throws IOException {
+		WebDriver driver = testContextSetup.testBase.WebDriverManager();
+		if(scenario.isFailed()) {
+			File sourcePath = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			byte[] fileContent = FileUtils.readFileToByteArray(sourcePath);
+			scenario.attach(fileContent, "image/png", "screenshot");
+		}
+	}
 	
 
 }
